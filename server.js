7db99aa7c66408b63a8d82bd82cc50d769e2d8d4
Johnny-Zou <johnny.zou@mail.utoronto.ca>
2018-05-18 23:39:43 -0600

    changed server port

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var path = require('path');
 var bodyParser = require('body-parser');
 
-var port = process.env.PORT;
+var port = 8000;
 
 // Routes
 var game = require('./routes/game.js');
