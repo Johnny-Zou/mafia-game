f20e98d5dd3c0154ba8d055274b59b9e5b6c68b5
Johnny-Zou <johnny.zou@mail.utoronto.ca>
2018-05-19 03:20:52 +0000

    added CRUD api

--- routes/api.js
+++ routes/api.js
@@ -1,9 +1,88 @@
 var express = require('express');
+var mongojs = require('mongojs');
+
 var router = express.Router();
 
-router.get('/',function(req,res,next){
-    res.send("arrived at api page");
+var db = mongojs('mongodb://mafia_admin:bestappever_mafia_admin123@ds145790.mlab.com:45790/mafia_db',['game']);
+
+//REST API
+
+// Get All Games
+router.get('/game', function(req, res, next){
+    db.game.find(function(err, game){
+        if(err){
+            res.send(err);
+        }
+        res.json(game);
+    });
 });
+
+// Get Single game
+router.get('/game/:id', function(req, res, next){
+    db.game.findOne({"game_id": mongojs.ObjectId(req.params.id)}, function(err, game){
+        if(err){
+            res.send(err);
+        }
+        res.json(game);
+    });
+});
+
+//Save game
+router.post('/game', function(req, res, next){
+    var game = req.body;
+    if(!game.game_id || !(game.player_num)){
+        res.status(400);
+        res.json({
+            "error": "Bad Data"
+        });
+    } else {
+        db.game.save(game, function(err, game){
+            if(err){
+                res.send(err);
+            }
+            res.json(game);
+        });
+    }
+});
+
+// Delete game
+router.delete('/game/:id', function(req, res, next){
+    db.game.remove({game_id: mongojs.ObjectId(req.params.id)}, function(err, game){
+        if(err){
+            res.send(err);
+        }
+        res.json(game);
+    });
+});
+
+// Update game
+router.put('/game/:id', function(req, res, next){
+    var game = req.body;
+    var updateGame = {};
+    
+    if(game.isDone){
+        updateGame.isDone = game.isDone;
+    }
+    
+    if(game.title){
+        updateGame.title = game.title;
+    }
+    
+    if(!updateGame){
+        res.status(400);
+        res.json({
+            "error":"Bad Data"
+        });
+    } else {
+        db.game.update({game_id: mongojs.ObjectId(req.params.id)},updateGame, {}, function(err, game){
+        if(err){
+            res.send(err);
+        }
+        res.json(game);
+    });
+    }
+});
+
 
 
 module.exports = router;
