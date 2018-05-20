67bf7cd91836690b304d4e6851d32622f6637411
Johnny-Zou <johnny.zou@mail.utoronto.ca>
2018-05-20 16:06:49 -0600

    finished CRUD api

--- routes/api.js
+++ routes/api.js
@@ -9,11 +9,11 @@ var db = mongojs('mongodb://mafia_admin:bestappever_mafia_admin123@ds145790.mlab
 
 // Get All Games
 router.get('/game', function(req, res, next){
-    db.game.find(function(err, allGames){
+    db.game.find(function(err, gameList){
         if(err){
             res.send(err);
         }
-        res.json(allGames);
+        res.json(gameList);
     });
 });
 
@@ -21,24 +21,31 @@ router.get('/game', function(req, res, next){
 router.get('/game/:id', function(req, res, next){
     console.log(req.params.id);
     console.log(typeof(req.params.id));
-    db.game.findOne({"game_id": parseInt(req.params.id)}, function(err, singleGame){
+    db.game.findOne({"game_id": parseInt(req.params.id)}, function(err, game){
         if(err){
             res.send(err);
         }
-        res.json(singleGame);
+        res.json(game);
     });
 });
 
-//Save game
+// Create a new game
 router.post('/game', function(req, res, next){
-    var game = req.body;
-    if(!game.game_id || !(game.player_num)){
+    var newGame = req.body;
+
+    //Check that all the fields are completed
+    var game_id_complete = !newGame.game_id;
+    var player_num_complete = !newGame.player_num;
+
+    var uncompletedFields = game_id_complete || player_num_complete;
+
+    if(uncompletedFields){
         res.status(400);
         res.json({
             "error": "Bad Data"
         });
     } else {
-        db.game.save(game, function(err, game){
+        db.game.save(newGame, function(err, game){
             if(err){
                 res.send(err);
             }
@@ -49,7 +56,7 @@ router.post('/game', function(req, res, next){
 
 // Delete game
 router.delete('/game/:id', function(req, res, next){
-    db.game.remove({game_id: mongojs.ObjectId(req.params.id)}, function(err, game){
+    db.game.remove({"game_id": parseInt(req.params.id)}, function(err, game){
         if(err){
             res.send(err);
         }
@@ -59,24 +66,24 @@ router.delete('/game/:id', function(req, res, next){
 
 // Update game
 router.put('/game/:id', function(req, res, next){
-    var game = req.body;
+    var gameInput = req.body;
     var updateGame = {};
     
-    if(game.isDone){
-        updateGame.isDone = game.isDone;
+    //Do not update the game ID
+
+    //Update the numebr of players
+    if(game.player_num){
+        updateGame.player_num = gameInput.player_num;
     }
-    
-    if(game.title){
-        updateGame.title = game.title;
-    }
-    
+
+    // Check empty
     if(!updateGame){
         res.status(400);
         res.json({
             "error":"Bad Data"
         });
     } else {
-        db.game.update({game_id: mongojs.ObjectId(req.params.id)},updateGame, {}, function(err, game){
+        db.game.update({"game_id": parseInt(req.params.id)}, updateGame, {}, function(err, game){
         if(err){
             res.send(err);
         }
